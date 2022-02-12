203b03561fdc9f6d3d79d502973efd5b416ef0ae
s3655612 <s3655612@student.rmit.edu.au>
2022-02-12 17:33:53 +1100

    Menu fades in and out on scroll

--- main.js
+++ main.js
@@ -1,9 +1,38 @@
 new fullpage('#fullpage', {
-    //options here
     licenseKey: 'YOUR KEY HERE',
     autoScrolling: true,
-    scrollHorizontally: true
+    scrollHorizontally: true,
+    scrollBar:true
 });
 
-// //methods
-// fullpage_api.setAllowScrolling(true);
+
+// $(window).scroll(function(){
+//     $('nav').toggleClass('scrolled', $(this).scrollTop() > 1080);
+//     });
+
+// document.getElementsByTagName('nav').addEventListener("scroll", myFunction);
+
+// function myFunction() {
+//     document.getElementsByClassName('navbar').toggleClass('scrolled', (this).scrollTop() > 1080);
+// }
+
+// document.getElementById("myDIV").onscroll = function() {myFunction()};
+
+// function myFunction() {
+//   document.getElementById("demo").innerHTML = "You scrolled in div.";
+// }
+
+
+// window.addEventListener('scroll', function(e) {
+//     document.getElementById('nav').classList[e.pageX > 1 ? 'add' : 'remove']('scrolled');
+// });
+
+window.onscroll = () => fadeNavBarBackground();
+
+function fadeNavBarBackground() {
+ if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+  document.getElementById("nav").classList.add("scrolled");
+ } else {
+  document.getElementById("nav").classList.remove("scrolled");
+ }
+}
