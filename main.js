af17b1592b291d16b73c10c3df5c8c512ad2d91c
s3655612 <s3655612@student.rmit.edu.au>
2022-02-13 19:42:46 +1100

    Adds scroll to section with click of navbar

--- main.js
+++ main.js
@@ -2,21 +2,19 @@ new fullpage('#fullpage', {
     licenseKey: 'YOUR KEY HERE',
     autoScrolling: true,
     scrollHorizontally: true,
-    scrollBar:true
+    scrollBar: true,
+    anchors: ['home', 'gallery', 'history', 'things-to-do', 'accom', 'getting-here'],
+    menu: '#nav-ul'
 });
 
 window.onscroll = () => fadeNavBarBackground();
 
 function fadeNavBarBackground() {
- if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-  document.getElementById("nav").classList.add("scrolled");
-  document.getElementById("brand").classList.add("scrolled");
- } else {
-  document.getElementById("nav").classList.remove("scrolled");
-  document.getElementById("brand").classList.remove("scrolled");
- }
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("nav").classList.add("scrolled");
+        document.getElementById("brand").classList.add("scrolled");
+    } else {
+        document.getElementById("nav").classList.remove("scrolled");
+        document.getElementById("brand").classList.remove("scrolled");
+    }
 }
-
-var scrollSpy = new bootstrap.ScrollSpy(document.body, {
-    target: '#navbar-example'
-  })
