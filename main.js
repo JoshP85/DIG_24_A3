9475754a3efa74495780cc1752ffad0a1edae60a
JoshP85 <joshua.phillips1985@gmail.com>
2022-02-23 22:05:12 +1100

    Comments

--- main.js
+++ main.js
@@ -31,11 +31,10 @@ function fadeNavBarBackground() {
 // Changes the hero image to night if the switch is clicked
 const photoSwitch = document.querySelector('#selTheme');
 
-photoSwitch.addEventListener('sl-change', () =>{
-    if(photoSwitch.checked === true){
+photoSwitch.addEventListener('sl-change', () => {
+    if (photoSwitch.checked === true) {
         document.querySelector('.hero').style.backgroundImage = "url('images/sublime-night.png')";
-    }
-    else{
+    } else {
         document.querySelector('.hero').style.backgroundImage = "url('images/bayley-clark-0v7BeBsQhCY-unsplash-widescreen.jpg')";
     }
 })
